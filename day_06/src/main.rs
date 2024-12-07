f23bc817eec2779df89f7bebe4e5805df54e4c46
Lucas Øverland <lucas.overland@gmail.com>
2024-12-07 00:23:10 -0600

    remove useless hashing

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -8,7 +8,7 @@ enum Direction {
     W,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct Guard {
     dir: Direction,
     pos: (isize, isize),
@@ -22,10 +22,6 @@ impl Guard {
             Direction::S => (self.pos.0, self.pos.1 - 1),
             Direction::W => (self.pos.0 - 1, self.pos.1),
         }
-    }
-
-    fn hash(&self) -> (Direction, (isize, isize)) {
-        (self.dir, self.pos)
     }
 
     fn ghost_step(&self, rigids: &HashSet<(isize, isize)>) -> Self {
@@ -120,21 +116,23 @@ fn part_two(input: &str) -> usize {
     let mut paces = HashSet::new();
     let mut paces_pos = HashSet::new();
     while inbounds(&guard, max_x, max_y) {
-        paces.insert(guard.hash());
+        paces.insert(guard.clone());
         paces_pos.insert(guard.pos);
 
         let obstacle = guard.front();
 
-        if !obstacles.contains(&obstacle) && !rigids.contains(&obstacle) && !paces_pos.contains(&obstacle) {
+        if !obstacles.contains(&obstacle)
+            && !rigids.contains(&obstacle)
+            && !paces_pos.contains(&obstacle)
+        {
             let mut ghost_paces = HashSet::new();
             let mut ghost_guard = guard.ghost_step(&rigids);
             while inbounds(&ghost_guard, max_x, max_y) {
-                if paces.contains(&ghost_guard.hash()) || ghost_paces.contains(&ghost_guard.hash())
-                {
+                if paces.contains(&ghost_guard) || ghost_paces.contains(&ghost_guard) {
                     obstacles.insert(obstacle);
                     break;
                 } else {
-                    ghost_paces.insert(ghost_guard.hash());
+                    ghost_paces.insert(ghost_guard.clone());
                     ghost_guard.step(&rigids, Some(obstacle));
                 }
             }
@@ -154,12 +152,6 @@ fn main() {
 
     println!("Part One: {}", part_one(input));
     println!("Part Two: {}", part_two(input));
-    // 5780 too high
-    // 1725 too high
-    // 1722 too high
-    // 1675 WRONG
-    // 1657 WRONG
-    // 1648 WRONG
 }
 
 #[cfg(test)]
